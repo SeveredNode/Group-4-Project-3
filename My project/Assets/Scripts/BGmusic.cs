6817130076fa6f774617c8f120245460b21aaabe
tristan-33 <tristan.munoz33@gmail.com>
2022-11-15 03:31:20 -0800

    Added music and music script. Also added robert to credits and created a build

---
+++ My project/Assets/Scripts/BGmusic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGmusic : MonoBehaviour
+{
+    public static BGmusic instance;
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "MainScene")
+            BGmusic.instance.GetComponent<AudioSource>().Pause();
+    }
+}
